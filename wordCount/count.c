fb97083751315000293995c67ab8e9dcb3419eaf
yyma <70613116+Yiyao-Ma@users.noreply.github.com>
2025-01-06 21:25:58 +0800

    Add files via upload

---
+++ wordCount/count.c
@@ -0,0 +1,11 @@
+/***********************************
+* Program code for count()
+***********************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include "util.h"
+
+int count(int argc,char *argv[])
+{
+	/* Write Your Code HERE! */
+}
